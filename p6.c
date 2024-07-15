7e9764db3fbfd073fa7a5cce5b3b99a794c4524d
Siddhanth P <siddhantprsd15@gmail.com>
2024-07-15 14:45:48 +0530

    Update p6.c

--- p6.c
+++ p6.c
@@ -1,169 +1,128 @@
 #include <GL/glut.h>
-#include <stdio.h>
+#include <stdbool.h>  // Include for bool, true, false
 #include <math.h>
-#include <stdlib.h>
 
-#define PI 3.1416
+const float PI = 3.14159265f;
+GLfloat ballRadius = 0.1f;
+GLfloat ballX = 0.0f;
+GLfloat ballY = 0.0f;
+GLfloat ballXMax, ballXMin, ballYMax, ballYMin;
+GLfloat xSpeed = 0.008f;
+GLfloat ySpeed = 0.01f;
+GLfloat xAcceleration = 0.0000f;
+GLfloat yAcceleration = -0.0005f;
+bool isPaused = false;
+int refreshMillis = 16;
+GLdouble XLeft, XRight, YBottom, YTop;
 
-static int win, val = 0, CMenu;
-
-void CreateGLMenu(void);
-void Menu(int value);
-
-struct wcPt3D {
-    GLfloat x, y, z;
-};
-
-GLsizei winWidth = 600, winHeight = 600;
-GLfloat xwcMin = 0.0, xwcMax = 130.0;
-GLfloat ywcMin = 0.0, ywcMax = 130.0;
-
-void bino(GLint n, GLint *C) {
-    GLint k, j;
-    for (k = 0; k <= n; k++) {
-        C[k] = 1;
-        for (j = n; j >= k + 1; j--)
-            C[k] *= j;
-        for (j = n - k; j >= 2; j--)
-            C[k] /= j;
-    }
+void init()
+{
+    glClearColor(0.0, 0.0, 0.0, 1.0);
 }
 
-void computeBezPt(GLfloat u, struct wcPt3D *bezPt, GLint nCtrlPts, struct wcPt3D *ctrlPts, GLint *C) {
-    GLint k, n = nCtrlPts - 1;
-    GLfloat bezBlendFcn;
-    bezPt->x = bezPt->y = bezPt->z = 0.0;
-    for (k = 0; k < nCtrlPts; k++) {
-        bezBlendFcn = C[k] * pow(u, k) * pow(1 - u, n - k);
-        bezPt->x += ctrlPts[k].x * bezBlendFcn;
-        bezPt->y += ctrlPts[k].y * bezBlendFcn;
-        bezPt->z += ctrlPts[k].z * bezBlendFcn;
-    }
-}
-
-void bezier(struct wcPt3D *ctrlPts, GLint nCtrlPts, GLint nBezCurvePts) {
-    struct wcPt3D bezCurvePt;
-    GLfloat u;
-    GLint *C, k;
-    C = (GLint*)malloc(nCtrlPts * sizeof(GLint));
-    bino(nCtrlPts - 1, C);
-    glBegin(GL_LINE_STRIP);
-    for (k = 0; k <= nBezCurvePts; k++) {
-        u = (GLfloat)k / (GLfloat)nBezCurvePts;
-        computeBezPt(u, &bezCurvePt, nCtrlPts, ctrlPts, C);
-        glVertex2f(bezCurvePt.x, bezCurvePt.y);
+void display()
+{
+    if (isPaused) return;
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(ballX, ballY, 0.0f);
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex2f(0.0f, 0.0f);
+    int numSegments = 100;
+    GLfloat angle;
+    for (int i = 0; i <= numSegments; i++)
+    {
+        angle = i * 2.0f * PI / numSegments;
+        glVertex2f(cos(angle) * ballRadius, sin(angle) * ballRadius);
     }
     glEnd();
-    free(C);
-}
-
-void displayFcn() {
-    GLint nCtrlPts = 4, nBezCurvePts = 20;
-    static float theta = 0;
-    struct wcPt3D ctrlPts[4] = {{20, 100, 0}, {30, 110, 0}, {50, 90, 0}, {60, 100, 0}};
-    ctrlPts[1].x += 10 * sin(theta * PI / 180.0);
-    ctrlPts[1].y += 5 * sin(theta * PI / 180.0);
-    ctrlPts[2].x -= 10 * sin((theta + 30) * PI / 180.0);
-    ctrlPts[2].y -= 10 * sin((theta + 30) * PI / 180.0);
-    ctrlPts[3].x -= 4 * sin(theta * PI / 180.0);
-    ctrlPts[3].y += sin((theta - 30) * PI / 180.0);
-    theta += 0.1;
-
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0, 1.0, 1.0);
-    glPointSize(5);
-
-    // Indian Flag
-    if (val == 1) {
-        glPushMatrix();
-        glLineWidth(5);
-        glColor3f(1.0, 0.5, 0); // Indian flag: Orange color code
-        for (int i = 0; i < 8; i++) {
-            glTranslatef(0, -0.8, 0);
-            bezier(ctrlPts, nCtrlPts, nBezCurvePts);
-        }
-        glColor3f(1, 1, 1); // Indian flag: white color code
-        for (int i = 0; i < 8; i++) {
-            glTranslatef(0, -0.8, 0);
-            bezier(ctrlPts, nCtrlPts, nBezCurvePts);
-        }
-        glColor3f(0, 1.0, 0); // Indian flag: green color code
-        for (int i = 0; i < 8; i++) {
-            glTranslatef(0, -0.8, 0);
-            bezier(ctrlPts, nCtrlPts, nBezCurvePts);
-        }
-        glPopMatrix();
-        glColor3f(0.7, 0.5, 0.3);
-        glLineWidth(5);
-        glBegin(GL_LINES);
-        glVertex2f(20, 100);
-        glVertex2f(20, 40);
-        glEnd();
-        glFlush();
-    }
-
-    // Karnataka Flag
-    if (val == 2) {
-        glPushMatrix();
-        glLineWidth(5);
-        glColor3f(1.0, 1.0, 0.0); // Karnataka flag: Yellow color code
-        for (int i = 0; i < 12; i++) {
-            glTranslatef(0, -0.8, 0);
-            bezier(ctrlPts, nCtrlPts, nBezCurvePts);
-        }
-        glColor3f(1, 0.0, 0.0); // Karnataka flag: Red color code
-        for (int i = 0; i < 12; i++) {
-            glTranslatef(0, -0.8, 0);
-            bezier(ctrlPts, nCtrlPts, nBezCurvePts);
-        }
-        glPopMatrix();
-        glColor3f(0.7, 0.5, 0.3);
-        glLineWidth(5);
-        glBegin(GL_LINES);
-        glVertex2f(20, 100);
-        glVertex2f(20, 40);
-        glEnd();
-        glFlush();
-    }
-
-    glutPostRedisplay();
     glutSwapBuffers();
+    xSpeed += xAcceleration;
+    ySpeed += yAcceleration;
+
+    ballX += xSpeed;
+    ballY += ySpeed;
+    if (ballX > ballXMax)
+    {
+        ballX = ballXMax;
+        xSpeed = -xSpeed;
+    }
+    else if (ballX < ballXMin)
+    {
+        ballX = ballXMin;
+        xSpeed = -xSpeed;
+    }
+    if (ballY > ballYMax)
+    {
+        ballY = ballYMax;
+        ySpeed = -ySpeed;
+    }
+    else if (ballY < ballYMin)
+    {
+        ballY = ballYMin;
+        ySpeed = -ySpeed;
+    }
 }
 
-void winReshapeFun(GLint newWidth, GLint newHeight) {
-    glViewport(0, 0, newWidth, newHeight);
+void reshape(GLsizei width, GLsizei height)
+{
+    if (height == 0) height = 1;
+    GLfloat aspect = (GLfloat)width / (GLfloat)height;
+    glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(xwcMin, xwcMax, ywcMin, ywcMax);
-    glClear(GL_COLOR_BUFFER_BIT);
+    if (width >= height)
+    {
+        XLeft = -1.0 * aspect;
+        XRight = 1.0 * aspect;
+        YBottom = -1.0;
+        YTop = 1.0;
+    }
+    else
+    {
+        XLeft = -1.0;
+        XRight = 1.0;
+        YBottom = -1.0 / aspect;
+        YTop = 1.0 / aspect;
+    }
+    gluOrtho2D(XLeft, XRight, YBottom, YTop);
+
+    ballXMin = XLeft + ballRadius;
+    ballXMax = XRight - ballRadius;
+    ballYMin = YBottom + ballRadius;
+    ballYMax = YTop - ballRadius;
 }
 
-void CreateGLMenu(void) {
-    CMenu = glutCreateMenu(Menu); // Create Menu Option
-    glutAddMenuEntry("Indian Flag", 1);
-    glutAddMenuEntry("Karnataka Flag", 2);
-    glutAddMenuEntry("Exit", 0);
-    glutAttachMenu(GLUT_RIGHT_BUTTON);
+void timer(int value)
+{
+    glutPostRedisplay();
+    glutTimerFunc(refreshMillis, timer, 0);
 }
 
-void Menu(int value) {
-    if (value == 0) {
-        glutDestroyWindow(win);
-        exit(0);
-    } else {
-        val = value;
+void keyboard(unsigned char key, int x, int y) {
+    switch (key)
+    {
+    case 32:
+        isPaused = !isPaused;
+        break;
+    default:
+        break;
     }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowPosition(50, 50);
-    glutInitWindowSize(winWidth, winHeight);
-    win = glutCreateWindow("Prg. 8 Bezier Curve");
-    CreateGLMenu();
-    glutDisplayFunc(displayFcn);
-    glutReshapeFunc(winReshapeFun);
+    glutInitDisplayMode(GLUT_DOUBLE);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("bouncing-ball");
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+    glutTimerFunc(0, timer, 0);
+    init();
     glutMainLoop();
     return 0;
 }
